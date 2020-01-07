1fe3b3f918358dd7044fb9a3651d5eb18a09e3c9
giuseppe <Giuseppearimatea10@gmail.com>
2020-01-07 16:22:03 +0100

    commit

---
+++ dadi/js/script.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente la sua email,
+
+// controlla che sia nella lista di chi può accedere,
+
+// stampa un messaggio appropriato
+
+
+
+var nomiEmail = ['giuseppearimatea10@gmail.com','giuseppearimatea11@gmail.com','giuseppearimatea12@gmail.com','giuseppearimatea13@gmail.com','giuseppearimatea14@gmail.com','giuseppearimatea15@gmail.com'];
+
+for (var i = 0; i < nomiEmail.length; i++) {
+  console.log(nomiEmail);
+}
---
+++ mail/js/script.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente la sua email,
+
+// controlla che sia nella lista di chi può accedere,
+
+// stampa un messaggio appropriato
+
+
+var inserisciEmail = prompt('inserisci un email');
+var nomiEmail = ['giuseppearimatea10@gmail.com', 'giuseppearimatea11@gmail.com', 'giuseppearimatea12@gmail.com', 'giuseppearimatea13@gmail.com', 'giuseppearimatea14@gmail.com', 'giuseppearimatea15@gmail.com'];
+console.log(nomiEmail);
+
+for (var i = 0; i < nomiEmail.length; i++) {
+  if (inserisciEmail == nomiEmail[i] ) {
+    console.log(inserisciEmail + 'puoi accedere');
+  }
+  else if (inserisciEmail != nomiEmail[i]) {
+    console.log('non puoi accedere')
+  }
+}
